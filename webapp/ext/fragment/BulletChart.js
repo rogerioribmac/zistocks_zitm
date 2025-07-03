0ff3369a8116f71342df51161edef296a5ccac88
ROGERIO RIBEIRO <rogerio.ribeiromacedo@ext.europarl.europa.eu>
2025-07-03 12:14:15 +0000

    commit

--- webapp/ext/fragment/BulletChart.js
+++ webapp/ext/fragment/BulletChart.js
@@ -20,10 +20,11 @@ sap.ui.define(["sap/m/MessageToast", "sap/ui/core/HTML"], function (MessageToast
                             sContent += `<div style='margin: 15px 30px 0 10px'>${sSvgIcon}<b style='margin-left:10px'>${aDataValues[0].value}</b></div>`;
                             sContent += `<div style='margin: 5px 30px 0 30px'>${aDataValues[1].name}<span style='float: right'>${aDataValues[1].value}</span></div>`;
                             sContent += `<div style='margin: 5px 30px 0 30px'>${aDataValues[3].name}<span style='float: right'>${aDataValues[3].value}</span></div>`;
-                            sContent += `<div style='margin: 5px 30px 0 30px'>${aDataValues[2].name}<span style='float: right'>${aDataValues[2].value}</span></div>`;
+                            sContent += `<div style='margin: 5px 30px 0 30px'>${aDataValues[2].name}<span style='float: right'>${-1 * aDataValues[2].value}</span></div>`;
                             sContent += `<div style='margin: 5px 30px 0 30px'>Unavailable<span style='float: right'>${oContext.getProperty("unavailableStock")}</span></div>`;
-                            sContent += `<div style='margin: 5px 30px 0 30px'>Commited<span style='float: right'>${oContext.getProperty("commitedStock")}</span></div>`;
-                            sContent += `<div style='margin: 5px 30px 15px 30px'>Purchased not Dlv.<span style='float: right'>${oContext.getProperty("purchaseNotDelivered")}</span></div>`;
+                            sContent += `<div style='margin: 5px 30px 0 30px'>Committed<span style='float: right'>${oContext.getProperty("commitedStock")}</span></div>`;
+                            sContent += `<div style='margin: 5px 30px 0 30px'>Purchased not Dlv.<span style='float: right'>${oContext.getProperty("purchaseNotDelivered")}</span></div>`;
+                            sContent += `<div style='margin: 5px 30px 15px 30px'>Available + Purchased not Dlv.<span style='float: right'>${oContext.getProperty("availableAndNotDelivered")}</span></div>`;
                         }
                         
                         return new HTML({ content: sContent });
