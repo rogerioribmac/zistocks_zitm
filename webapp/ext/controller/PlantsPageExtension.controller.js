a9cb8229b191730e487c49e508e7904a2900f3b2
ROGERIO RIBEIRO <rogerio.ribeiromacedo@ext.europarl.europa.eu>
2025-07-16 13:03:58 +0000

    commit

--- webapp/ext/controller/PlantsPageExtension.controller.js
+++ webapp/ext/controller/PlantsPageExtension.controller.js
@@ -23,9 +23,16 @@ sap.ui.define(['sap/ui/core/mvc/ControllerExtension'], function (ControllerExten
                     if (oHeaderContent) {
 
                         let sMaterialNo = oHeaderContent.getBindingContext().getProperty("materialNo");
+                        let sDG = oHeaderContent.getBindingContext().getProperty("DG");
                         if (sMaterialNo) {
 
-                            var oFilter = new sap.ui.model.Filter("materialNo", sap.ui.model.FilterOperator.EQ, sMaterialNo);
+                            var oFilter = new sap.ui.model.Filter({
+                                filters: [
+                                    new sap.ui.model.Filter("materialNo", sap.ui.model.FilterOperator.EQ, sMaterialNo),
+                                    new sap.ui.model.Filter("DG", sap.ui.model.FilterOperator.EQ, sDG)
+                                ],
+                                and: true
+                            });
                             oChart.getDataset().getBinding("data").filter();
                             oChart.getDataset().getBinding("data").filter(oFilter);
                             oChart.getDataset().updateBindings(true);
